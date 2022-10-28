c298917293673548b72e2dba0451910df9e16850
Alicia Wonderland <aliciafsd42@gmail.com>
2022-10-28 16:29:57 -0400

    power

--- app.js
+++ app.js
@@ -15,4 +15,8 @@ app.get('/multiply/:a/:b', (req, res) =>
   res.send((parseInt(req.params.a) * parseInt(req.params.b)).toString())
 )
 
+app.get('/power/:a/:b', (req, res) =>
+  res.send(Math.pow(parseInt(req.params.a), parseInt(req.params.b)).toString())
+)
+
 app.listen(3000)
